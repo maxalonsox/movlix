d4992154817007d364a690f84c0d56154b097a0c
Maximiliano Alonso <maxalonso@alumnos.exa.unicen.edu.ar>
2022-12-01 15:35:01 +0100

    complete component director-list and fix styles

---
+++ src/app/director-list/Director.ts
@@ -0,0 +1,6 @@
+export interface Director {
+    name: string;
+    movies: string;
+    image: string;
+    link: string;
+}
--- src/app/director-list/director-list.component.ts
+++ src/app/director-list/director-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Director } from './Director';
 
 @Component({
   selector: 'app-director-list',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class DirectorListComponent {
 
+  directors: Director[] = [
+    {
+      name : "Christopher Nolan",
+      movies : "Inception, Batman Dark Knight, Interestellar...",
+      image : "https://i.ibb.co/NKmrPzS/christopher-nolan.jpg",
+      link : "https://es.wikipedia.org/wiki/Christopher_Nolan"
+    },
+    {
+      name : "James Cameron",
+      movies : "Avatar, Titanic, Terminator...",
+      image : "https://i.ibb.co/18SH4SS/james-cameron.jpg",
+      link: "https://es.wikipedia.org/wiki/James_Cameron"
+    },
+    {
+      name : "Quentin Tarantino",
+      movies : "Kill Bill, Django sin cadenas, Los 8 más odiados...",
+      image : "https://i.ibb.co/X5MsWT0/quentin-tarantino.jpg",
+      link : "https://es.wikipedia.org/wiki/Quentin_Tarantino"
+    },
+  ]
 }
